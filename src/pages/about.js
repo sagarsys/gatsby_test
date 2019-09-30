220e987b49023b9f363745ad5723c5fec0a1bc07
sagar <sagar_sys@live.com>
2019-09-30 07:15:02 +0400

    part 2 - styles

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,33 @@
 import React from "react"
-import Header from "../components/header";
+import styles from "./about.module.css";
+import Header from "../components/header"
+import Container from "../components/container"
 import { Link } from "gatsby"
 
+const User = props => (
+  <div className={styles.user}>
+    <img src={props.avatar} className={styles.avatar} alt="" />
+    <div className={styles.description}>
+      <h2 className={styles.username}>{props.username}</h2>
+      <p className={styles.excerpt}>{props.excerpt}</p>
+    </div>
+  </div>
+)
+
 export default () => (
-  <div style={{ color: `teal` }}>
+  <Container>
     <Link to="/">Home</Link>
     <Header headerText="About Gatsby" />
-    <Header headerText="It's pretty cool" />
     <p>Such wow. Very React.</p>
-  </div>
+    <User
+      username="Jane Doe"
+      avatar="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
+      excerpt="I'm Jane Doe. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    />
+    <User
+      username="Bob Smith"
+      avatar="https://s3.amazonaws.com/uifaces/faces/twitter/vladarbatov/128.jpg"
+      excerpt="I'm Bob Smith, a vertically aligned type of guy. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    />
+  </Container>
 )
